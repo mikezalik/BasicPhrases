661ba011d08d7bcf0efcd91e268a700a3faa3dc5
mikezalik <mike.zalik@gmail.com>
2020-01-23 09:30:51 -0800

    Adds onClick to buttons

--- app/src/main/java/com/mikezalik/basicphrases/MainActivity.java
+++ app/src/main/java/com/mikezalik/basicphrases/MainActivity.java
@@ -3,8 +3,15 @@ package com.mikezalik.basicphrases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
